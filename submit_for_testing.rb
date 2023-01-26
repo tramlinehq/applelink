1a3c955cd714a877452065a9db9351ee2b8f61c2
Nivedita <nid.mishra7@gmail.com>
2023-01-26 23:21:36 +0530

    Add submit to beta group

--- submit_for_testing.rb
+++ submit_for_testing.rb
@@ -5,3 +5,18 @@ set_auth_token
 # Find app by bundle id
 app = Spaceship::ConnectAPI::App.find(BID)
 
+build = Spaceship::ConnectAPI::Build.all(
+  app_id: app.id,
+  version: "7.0.0", # input from user
+  build_number: "1002" # input from user
+).first
+
+build_data(build)
+
+beta_group = app.get_beta_groups(filter: { name: "Opinions" }).first
+
+puts beta_group.id
+
+puts "Adding build to beta group - #{beta_group.name}"
+
+build.add_beta_groups(beta_groups: [beta_group])
