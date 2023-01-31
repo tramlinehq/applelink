94e2ef4553122d695576233f8352f852d6ffb38d
Akshay Gupta <kitallis@gmail.com>
2023-01-31 14:26:42 +0530

    Reduce sentry sample rate

--- initializers/sentry_config.rb
+++ initializers/sentry_config.rb
@@ -7,14 +7,7 @@ module Initializers
       config.dsn = ENV["SENTRY_DSN"]
       config.breadcrumbs_logger = [:sentry_logger, :http_logger]
       config.enabled_environments = %w[production]
-
-      # To activate performance monitoring, set one of these options.
-      # We recommend adjusting the value in production:
-      config.traces_sample_rate = 1.0
-      # or
-      config.traces_sampler = lambda do |context|
-        true
-      end
+      config.traces_sample_rate = 0.7
     end
   end
 end
