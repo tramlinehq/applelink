02a22a3349745f00962c0ade5976d28dc7ea8cce
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-07-05 20:00:49 +0530

    lint

--- lib/app_store/connect.rb
+++ lib/app_store/connect.rb
@@ -224,12 +224,12 @@ module AppStore
 
         if build_number.nil? || build_number.empty?
           version = app.get_app_store_versions(includes: VERSION_DATA_INCLUDES, filter:)
-                       .max_by { |v| Date.parse(v.created_date) }
+            .max_by { |v| Date.parse(v.created_date) }
 
           raise VersionNotFoundError.new("No inflight release found") unless version
         else
           version = app.get_app_store_versions(includes: VERSION_DATA_INCLUDES, filter:)
-                       .find { |v| v.build&.version == build_number }
+            .find { |v| v.build&.version == build_number }
 
           raise VersionNotFoundError.new("No release found for the build number - #{build_number}") unless version
         end
