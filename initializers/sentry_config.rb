4b7cf2c661b2be952435abed7eb41072d305b8d6
Akshay Gupta <kitallis@gmail.com>
2024-05-24 13:59:28 +0530

    Disable sampling rate for staging

--- initializers/sentry_config.rb
+++ initializers/sentry_config.rb
@@ -7,7 +7,11 @@ module Initializers
       config.dsn = ENV["SENTRY_DSN"]
       config.breadcrumbs_logger = [:sentry_logger, :http_logger]
       config.enabled_environments = %w[production]
-      config.traces_sample_rate = 0.2
+      # config.traces_sample_rate = 0.2
+      config.traces_sampler = lambda do |_|
+        return 0.0 if ENV["RACK_ENV"].eql?("staging")
+        0.2
+      end
       config.logger.level = Logger::WARN
     end
   end
