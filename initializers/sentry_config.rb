a923081cfb8fdc4e3ffac45d384230afa8f4f792
Akshay Gupta <kitallis@gmail.com>
2024-11-20 14:18:20 +0530

    Use a uniform sample rate for Sentry
    Signed-off-by: Akshay Gupta <kitallis@gmail.com>

--- initializers/sentry_config.rb
+++ initializers/sentry_config.rb
@@ -7,11 +7,7 @@ module Initializers
       config.dsn = ENV["SENTRY_DSN"]
       config.breadcrumbs_logger = [:sentry_logger, :http_logger]
       config.enabled_environments = %w[production]
-      # config.traces_sample_rate = 0.2
-      config.traces_sampler = lambda do |_|
-        return 0.0 if ENV["RACK_ENV"].eql?("staging")
-        0.2
-      end
+      config.traces_sample_rate = (ENV["RACK_ENV"].eql?("staging") ? 0.0 : 0.2)
       config.logger.level = Logger::WARN
     end
   end
